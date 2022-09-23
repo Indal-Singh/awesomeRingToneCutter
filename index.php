36885bf0f08e17e705f73f67359bd0c483b3bd9b
Indal Singh <ind9546@gmail.com>
2022-09-23 13:16:37 +0530

    some changes on 23 sept 2022

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,24 +9,30 @@
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <header>
         <h2>Ringtone Genarator</h2>
     </header>
     <main>
-    <div class="uploaderBox">
-        <div class="up-row">
-            <div class="up-col selected">Upload</div>
-            <div class="up-col">URL</div>
+        <div class="uploaderBox">
+            <div class="up-row">
+                <div class="up-col selected" data-type="File">Upload</div>
+                <div class="up-col" data-type="URL">URL</div>
+            </div>
+            <div class="up-contollers">
+                <div id="inputFile">
+                    <input type="file" accept="audio/*" class="sourceFile" id="ringtoneFile">
+                    <label for="ringtoneFile">Select File..</label>
+                </div>
+                <div id="inputURL">
+                    <input type="text" class="sourceFile" placeholder="Paste Your Audio Url....." class="sourceFile">
+                </div>
+            </div>
         </div>
-        <div class="up-contollers">
-            <input type="file" accept="audio/*" id="ringtoneFile">
-            <label for="ringtoneFile">Select File..</label>
-            <input type="text" id="ringtoneFile">
-        </div>
-    </div>
     </main>
 
     <script src="js/main.js"></script>
 </body>
+
 </html>
--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 let selectType = (e) => // function foe upload type upload file or URL
 {
-    // console.log(e);
+
     $('.up-col').removeClass('selected');
     e.target.classList.add('selected');
+    let type=e.target.getAttribute('data-type'); // get which button clicked by user
+    // console.log(type);
+    if(type=='File')
+    {
+        $('#inputURL').hide();  
+        $('#inputFile').fadeIn();
+    }
+    else if(type=='URL')
+    {
+        $('#inputFile').hide();
+        $('#inputURL').fadeIn();
+    }
 }
 
-let loadFile = () => // loading file
+let loadFile = (e) => // loading file
 {
-    // fileInput = 
+    if(e.target.files)
+    {
+        let fileInput = e.target.files[0];
+        $('label').html(fileInput.name);
+        // console.log(e.target.files);
+    }
+    
 }
 
 $('.up-col').on('click',selectType) // calling on click
+$('.sourceFile').on('change',loadFile) // calling on click
+
+
+
+
+
+
 
